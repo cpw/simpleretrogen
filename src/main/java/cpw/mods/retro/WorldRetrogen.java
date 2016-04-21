7336ee1e5da42f04a682d9f8d8ab5e4c66276274
cpw <cpw@weeksfamily.ca>
2016-04-21 00:35:44 -0400

    Update to 1.9 1865 forge. Add a command to list retrogen targets. Reorganize config file so you can have multiple retrogen sections with different markers.

--- src/main/java/cpw/mods/retro/WorldRetrogen.java
+++ src/main/java/cpw/mods/retro/WorldRetrogen.java
@@ -23,17 +23,27 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Lists;
 import com.google.common.collect.MapMaker;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.google.common.collect.Sets.SetView;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.nbt.NBTTagString;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.ChunkCoordIntPair;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.ChunkProviderServer;
 import net.minecraftforge.common.MinecraftForge;
@@ -47,10 +57,12 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.event.FMLServerStoppedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import org.apache.logging.log4j.Level;
 
 import java.util.Iterator;
 import java.util.List;
@@ -62,12 +74,9 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Semaphore;
 
-@Mod(modid="simpleretrogen", name="Simple Retrogen")
+@Mod(modid="simpleretrogen", name="Simple Retrogen", acceptableRemoteVersions="*")
 public class WorldRetrogen {
-    private Set<String> retros;
-
-    private String marker;
-
+    private List<Marker> markers = Lists.newArrayList();
     private Map<String,TargetWorldWrapper> delegates;
 
     private Map<World,ListMultimap<ChunkCoordIntPair,String>> pendingWork;
@@ -76,23 +85,74 @@ public class WorldRetrogen {
     private ConcurrentMap<World,Semaphore> completedWorkLocks;
 
     private int maxPerTick;
+    private Map<String,String> retros = Maps.newHashMap();
+
+    private static class Marker {
+        private final String marker;
+        private final Set<String> classes;
+
+        Marker(String marker, Set<String> classes) {
+            this.marker = marker;
+            this.classes = classes;
+        }
+    }
     @EventHandler
     public void preInit(FMLPreInitializationEvent evt)
     {
-        Configuration cfg = new Configuration(evt.getSuggestedConfigurationFile());
+        Configuration cfg = new Configuration(evt.getSuggestedConfigurationFile(), null, true);
         cfg.load();
-        Property property = cfg.get(Configuration.CATEGORY_GENERAL, "worldGens", new String[0]);
-        property.comment = "List of IWorldGenerator instances to fire retrogen for";
-        String[] retros = property.getStringList();
-        this.retros = Sets.newHashSet(retros);
 
-        property = cfg.get(Configuration.CATEGORY_GENERAL, "marker", "CPWRGMARK");
-        property.comment = "Marker to apply to chunks to indicate retrogen occurred";
-        this.marker = property.getString();
-
-        property = cfg.get(Configuration.CATEGORY_GENERAL, "maxPerTick", 100);
-        property.comment = "Maximum number of retrogens to run in a single tick";
+        Property property = cfg.get(Configuration.CATEGORY_GENERAL, "maxPerTick", 100);
+        property.setComment("Maximum number of retrogens to run in a single tick");
         this.maxPerTick = property.getInt(100);
+        Property amProperty = cfg.get(Configuration.CATEGORY_GENERAL, "markerList", new String[0]);
+        amProperty.setComment("Active markers");
+        final List<String> activeMarkerList = Lists.newArrayList(amProperty.getStringList());
+        Set<String> categories = cfg.getCategoryNames();
+
+        if (categories.size() == 1) // only the general category - version 1 of config file
+        {
+            property = cfg.get(Configuration.CATEGORY_GENERAL, "worldGens", new String[0]);
+            String[] retros = property.getStringList();
+            property = cfg.get(Configuration.CATEGORY_GENERAL, "marker", "CPWRGMARK");
+            Marker m = new Marker(property.getString(), Sets.newHashSet(retros));
+            this.markers.add(m);
+        }
+        else
+        {
+            for (String marker : activeMarkerList) {
+                if (categories.contains(marker)) {
+                    final Property property1 = cfg.get(marker, "worldGens", new String[0]);
+                    property1.setComment("World Generator classes for marker");
+                    this.markers.add(new Marker(marker, Sets.newHashSet(property1.getStringList())));
+                    cfg.getCategory(marker).setComment("Marker definition\nYou can create as many of these as you wish\nActivate by adding to active list");
+                } else {
+                    evt.getModLog().log(Level.INFO, "Ignoring missing marker definition for active marker %s", marker);
+                }
+            }
+        }
+
+        // clean up leftovers
+        cfg.getCategory(Configuration.CATEGORY_GENERAL).remove("worldGens");
+        cfg.getCategory(Configuration.CATEGORY_GENERAL).remove("marker");
+
+        for (Marker m : markers) {
+            for (String clz : m.classes) {
+                if (retros.put(clz, m.marker) != null) {
+                    evt.getModLog().log(Level.ERROR, "Configuration error, duplicate class for multiple markers found : %s", clz);
+                }
+            }
+            if (!categories.contains(m.marker)) {
+                Property p = cfg.get(m.marker, "worldGens",new String[0]);
+                p.setComment("World Generator classes for marker");
+                p.set(m.classes.toArray(new String[0]));
+                cfg.getCategory(m.marker).setComment("Marker definition\nYou can create as many of these as you wish\nActivate by adding to active list");
+                if (!activeMarkerList.contains(m.marker)) {
+                    activeMarkerList.add(m.marker);
+                    amProperty.set(activeMarkerList.toArray(new String[0]));
+                }
+            }
+        }
 
         if (cfg.hasChanged())
         {
@@ -105,6 +165,47 @@ public class WorldRetrogen {
     }
 
     @EventHandler
+    public void serverStarting(FMLServerStartingEvent evt) {
+        evt.registerServerCommand(new CommandBase()
+        {
+            @Override
+            public String getCommandName()
+            {
+                return "listretrogenclasstargets";
+            }
+
+            @Override
+            public String getCommandUsage(ICommandSender sender)
+            {
+                return "List retrogens";
+            }
+
+            @Override
+            public int getRequiredPermissionLevel()
+            {
+                return 0;
+            }
+
+            @Override
+            public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
+            {
+                Set<IWorldGenerator> worldGens = ObfuscationReflectionHelper.getPrivateValue(GameRegistry.class, null, "worldGenerators");
+                List<String> targets = Lists.newArrayList();
+                for (IWorldGenerator worldGen : worldGens) {
+                    if (!(worldGen instanceof TargetWorldWrapper))
+                    {
+                        targets.add(worldGen.getClass().getName());
+                    }
+                }
+                if (targets.isEmpty()) {
+                    sender.addChatMessage(new TextComponentString("There are no retrogen target classes"));
+                } else {
+                    sender.addChatMessage(new TextComponentString(CommandBase.joinNiceStringFromCollection(targets)));
+                }
+            }
+        });
+    }
+    @EventHandler
     public void serverAboutToStart(FMLServerAboutToStartEvent evt)
     {
         this.pendingWork = new MapMaker().weakKeys().makeMap();
@@ -114,7 +215,7 @@ public class WorldRetrogen {
         Set<IWorldGenerator> worldGens = ObfuscationReflectionHelper.getPrivateValue(GameRegistry.class, null, "worldGenerators");
         Map<IWorldGenerator,Integer> worldGenIdx = ObfuscationReflectionHelper.getPrivateValue(GameRegistry.class, null, "worldGeneratorIndex");
 
-        for (String retro : ImmutableSet.copyOf(retros))
+        for (String retro : ImmutableSet.copyOf(retros.keySet()))
         {
             if (!delegates.containsKey(retro))
             {
@@ -208,8 +309,9 @@ public class WorldRetrogen {
     private class TargetWorldWrapper implements IWorldGenerator {
         private IWorldGenerator delegate;
         private String tag;
+
         @Override
-        public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
+        public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider)
         {
             FMLLog.fine("Passing generation for %s through to underlying generator", tag);
             delegate.generate(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider);
@@ -221,7 +323,7 @@ public class WorldRetrogen {
     @SubscribeEvent
     public void onChunkLoad(ChunkDataEvent.Load chunkevt)
     {
-        World w = chunkevt.world;
+        World w = chunkevt.getWorld();
         if (!(w instanceof WorldServer))
         {
             return;
@@ -231,17 +333,20 @@ public class WorldRetrogen {
         Chunk chk = chunkevt.getChunk();
         Set<String> existingGens = Sets.newHashSet();
         NBTTagCompound data = chunkevt.getData();
-        NBTTagCompound marker = data.getCompoundTag(this.marker);
-        NBTTagList tagList = marker.getTagList("list",8);
-        for (int i = 0; i < tagList.tagCount(); i++)
+        for (Marker m : markers)
         {
-            existingGens.add(tagList.getStringTagAt(i));
-        }
+            NBTTagCompound marker = data.getCompoundTag(m.marker);
+            NBTTagList tagList = marker.getTagList("list", 8);
+            for (int i = 0; i < tagList.tagCount(); i++)
+            {
+                existingGens.add(tagList.getStringTagAt(i));
+            }
 
-        SetView<String> difference = Sets.difference(retros, existingGens);
-        for (String retro : difference)
-        {
-            queueRetrogen(retro, w, chk.getChunkCoordIntPair());
+            SetView<String> difference = Sets.difference(m.classes, existingGens);
+            for (String retro : difference)
+            {
+                queueRetrogen(retro, w, chk.getChunkCoordIntPair());
+            }
         }
 
         for (String retro : existingGens)
@@ -253,7 +358,7 @@ public class WorldRetrogen {
     @SubscribeEvent
     public void onChunkSave(ChunkDataEvent.Save chunkevt)
     {
-        World w = chunkevt.world;
+        World w = chunkevt.getWorld();
         if (!(w instanceof WorldServer))
         {
             return;
@@ -264,17 +369,23 @@ public class WorldRetrogen {
             if (completedWork.containsKey(w))
             {
                 ListMultimap<ChunkCoordIntPair, String> doneChunks = completedWork.get(w);
-                List<String> list = doneChunks.get(chunkevt.getChunk().getChunkCoordIntPair());
-                if (list.isEmpty())
+                List<String> retroClassList = doneChunks.get(chunkevt.getChunk().getChunkCoordIntPair());
+                if (retroClassList.isEmpty())
                     return;
                 NBTTagCompound data = chunkevt.getData();
-                NBTTagCompound retro = new NBTTagCompound();
-                NBTTagList lst = new NBTTagList();
-                retro.setTag("list", lst);
-                data.setTag(this.marker, retro);
-                for (String retrogen : list)
+                for (String retroClass : retroClassList)
                 {
-                    lst.appendTag(new NBTTagString(retrogen));
+                    String marker = retros.get(retroClass);
+                    NBTTagList lst;
+                    if (data.hasKey(marker)) {
+                        lst = data.getCompoundTag(marker).getTagList("list", 8);
+                    } else {
+                        NBTTagCompound retro = new NBTTagCompound();
+                        lst = new NBTTagList();
+                        retro.setTag("list", lst);
+                        data.setTag(marker, retro);
+                    }
+                    lst.appendTag(new NBTTagString(retroClass));
                 }
             }
         }
@@ -298,12 +409,12 @@ public class WorldRetrogen {
             currentWork.put(chunkCoords, retro);
         }
     }
-    private void completeRetrogen(ChunkCoordIntPair chunkCoords, World world, String marker)
+    private void completeRetrogen(ChunkCoordIntPair chunkCoords, World world, String retroClass)
     {
         ListMultimap<ChunkCoordIntPair, String> pendingMap = pendingWork.get(world);
         if (pendingMap != null && pendingMap.containsKey(chunkCoords))
         {
-            pendingMap.remove(chunkCoords, marker);
+            pendingMap.remove(chunkCoords, retroClass);
         }
 
         getSemaphoreFor(world).acquireUninterruptibly();
@@ -316,7 +427,7 @@ public class WorldRetrogen {
                 completedWork.put(world, completedMap);
             }
 
-            completedMap.put(chunkCoords, marker);
+            completedMap.put(chunkCoords, retroClass);
         }
         finally
         {
@@ -324,7 +435,7 @@ public class WorldRetrogen {
         }
     }
 
-    private void runRetrogen(WorldServer world, ChunkCoordIntPair chunkCoords, String retro)
+    private void runRetrogen(WorldServer world, ChunkCoordIntPair chunkCoords, String retroClass)
     {
         long worldSeed = world.getSeed();
         Random fmlRandom = new Random(worldSeed);
@@ -333,10 +444,10 @@ public class WorldRetrogen {
         long chunkSeed = (xSeed * chunkCoords.chunkXPos + zSeed * chunkCoords.chunkZPos) ^ worldSeed;
 
         fmlRandom.setSeed(chunkSeed);
-        ChunkProviderServer providerServer = world.theChunkProviderServer;
-        IChunkProvider generator = ObfuscationReflectionHelper.getPrivateValue(ChunkProviderServer.class, providerServer, "field_73246_d", "currentChunkProvider");
-        delegates.get(retro).delegate.generate(fmlRandom, chunkCoords.chunkXPos, chunkCoords.chunkZPos, world, generator, providerServer);
-        FMLLog.fine("Retrogenerated chunk for %s", retro);
-        completeRetrogen(chunkCoords, world, retro);
+        ChunkProviderServer providerServer = world.getChunkProvider();
+        IChunkGenerator generator = ObfuscationReflectionHelper.getPrivateValue(ChunkProviderServer.class, providerServer, "field_186029_c", "chunkGenerator");
+        delegates.get(retroClass).delegate.generate(fmlRandom, chunkCoords.chunkXPos, chunkCoords.chunkZPos, world, generator, providerServer);
+        FMLLog.fine("Retrogenerated chunk for %s", retroClass);
+        completeRetrogen(chunkCoords, world, retroClass);
     }
 }
